ce72931989ea17e8f7ef1591907e0c3ec4d09506
Andrew <andrew-aralov@yandex.ru>
2021-09-05 12:47:37 +0300

    Updated setup.py and LICENSE.txt (now use MIT).

--- setup.py
+++ setup.py
@@ -11,15 +11,23 @@ tests_require = [
 ]
 
 setup(
-	name = 'Tensorflow learning rate live patcher',
-	version = '0.0.0.1',
+	name = 'tf_livepatch_lr',
+	version = '0.0.2',
+	url = 'https://github.com/andrewerf/tf_livepatch_lr',
 	author = 'Andrew Aralov',
 	author_email = 'andrew-aralov@yandex.ru',
 	packages = ['tf_livepatch_lr', 'tf_livepatch_lr.test'],
 	license = 'LICENSE.txt',
 	description = 'Simple project which allows you to change the learning rate schedule '
 				  'without stopping the training process (tensorflow only)',
+	long_description_content_type = 'text/markdown',
 	long_description = open('README.md').read(),
 	install_requires = install_requires,
-	tests_require = tests_require
+	tests_require = tests_require,
+
+	classifiers = [
+		'Programming Language :: Python :: 3 :: Only',
+		'License :: OSI Approved :: MIT License',
+		'Intended Audience :: Developers'
+	]
 )
